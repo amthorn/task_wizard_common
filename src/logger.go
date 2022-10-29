20a3b44a65e4f51dbf8bea45c2b174275f46cfef
Ava Thorn <avatheavian@gmail.com>
2022-10-28 21:22:04 -0400

    first commit

---
+++ src/logger.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	// log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+	log.Debug("Initialized logger")
+}
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.Debug("Initialized task_wizard_common")
+}
